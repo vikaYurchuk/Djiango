f0ed83cca209613f18166a94a4809c2655db2a85
vikaYurchuk <vikajurchyk@gmail.com>
2025-06-13 22:04:05 +0300

    update

--- main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-
-def index(request):
-    return render(request, "index.html")
-
-def catalog(request):
-    products = Product.objects.all()
-    return render(request, "catalog.html", {"products": products, "count": len(products)})
